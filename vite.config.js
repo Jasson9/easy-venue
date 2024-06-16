818edf71f176c810fe3ae00b2df8b1be8eabdd66
Jasson Widiarta <jasson.widiarta@binus.edu>
2024-06-16 23:30:19 +0700

    another attempt

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
+import { createRequire } from 'module'
+import path from 'path'
+const { resolve } = createRequire(import.meta.url)
 
+const prismaClient = `prisma${path.sep}client`
+
+const prismaClientIndexBrowser = resolve('@prisma/client/index-browser').replace(`@${prismaClient}`, `.${prismaClient}`)
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [sveltekit()],
-  resolve: {
-    alias: {
-      ".prisma/client/index-browser": "/node_modules/.prisma/client/index-browser.js"
-    }
-  }
+  resolve: { alias: { '.prisma/client/index-browser': path.relative(__dirname, prismaClientIndexBrowser) } },
 })
+// vite.config.ts
